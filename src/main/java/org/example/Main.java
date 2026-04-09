78165fd8d3e9e021b124f274f448c17ecf0d17e3
jws-create <jwsprings@gmail.com>
2026-04-09 16:23:52 -0400

    just some touches

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,24 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        while (true){
+            System.out.println("--- Financial Calculator ---");
+            System.out.println("1. mortgage Cal");
+            System.out.println("2. Future value Cal (compound interest");
+            System.out.println("3. Present value of Annuity cal");
+            System.out.println("0. Exit");
+            System.out.println("");
+            System.out.println("please choose options 1 - 3 to use calculator");
+            System.out.println("type 0 to exit");
+            String choice = sc.nextLine();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
         }
+
     }
 }
